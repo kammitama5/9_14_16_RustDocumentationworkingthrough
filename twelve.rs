61a66d676ac22721026a4d8f535b306a7827c9c3
kammitama5 <krystal.maughan@gmail.com>
2016-09-14 13:44:01 -0700

    2nd commit

---
+++ twelve.rs
@@ -0,0 +1,11 @@
+fn main() {
+	let a = [1, 2, 3, 4, 5, 6, 1];
+	let m = [4, 5, 6];
+	let names = ["Anna", "Bob", "Colin", "Dick"];
+	
+	println!("a has {} elements", a.len());
+	println!("m has {} elements", m.len());
+	println!("The second name is: {}", names[1]);
+	println!("The last name is: {}", names[3]);
+
+}
