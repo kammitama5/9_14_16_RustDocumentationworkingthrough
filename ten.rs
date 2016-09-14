0ad125ae200e379a19651674e14673241555c166
kammitama5 <krystal.maughan@gmail.com>
2016-09-14 13:35:18 -0700

    2nd commit

---
+++ ten.rs
@@ -0,0 +1,9 @@
+fn main()  {
+	let x = true;
+	let y = false;
+	let a = 'x';
+	let two_hearts = '💕';
+	println!("The value of x is {} and value of y is {}", x, y);
+	println!("The value of a is {} and the value of two_hearts is{}", 
+			a, two_hearts);
+}
