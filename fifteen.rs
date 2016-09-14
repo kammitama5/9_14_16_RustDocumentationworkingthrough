02052b4eeb8ded6c83a3e7f4dc884c81b9413e4d
kammitama5 <krystal.maughan@gmail.com>
2016-09-14 15:48:40 -0700

    2nd commit

---
+++ fifteen.rs
@@ -0,0 +1,13 @@
+fn main(){
+
+	let tuple = (3, 4, 5);
+	
+	let x = tuple.0;
+	let y = tuple.1;
+	let z = tuple.2;
+	
+	println!("x is {}", x);
+	println!("y is {}", y);
+	println!("z is {}", z);
+
+}
