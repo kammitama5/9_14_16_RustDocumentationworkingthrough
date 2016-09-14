9522c4cef9b5506100cbcbf23dba739f8fa7b926
kammitama5 <krystal.maughan@gmail.com>
2016-09-14 10:25:30 -0700

    2nd commit

---
+++ six.rs
@@ -0,0 +1,13 @@
+fn main(){
+	let mut x: i32 = 1;
+	x = 7;
+	let x = x;
+	
+	let mut y: i32 = 2;
+	let y = 4;
+	let y = "bound to text";
+	
+	println!("The value of x is {} and value of y is {}", x, y);
+
+
+}
