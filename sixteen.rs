9b564c3c782971c972766792018731a9851291c0
kammitama5 <krystal.maughan@gmail.com>
2016-09-14 15:56:51 -0700

    2nd commit

---
+++ sixteen.rs
@@ -0,0 +1,19 @@
+fn main(){
+	let x = "Bob";
+	
+	if x == "Sally"{
+		println!("x is Sally!");
+	}
+	else if x == "Wally"{
+		println!("y is Wally!");
+	}
+	else if x == "Bob"{
+		println!("Hi Bob!");
+	}
+	else {
+		println!("Nothing here!");
+	}
+	
+
+
+}
