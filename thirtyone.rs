9e84ab639a0c4fd44af3d82ab9fc1cdfb7f974ef
kammitama5 <krystal.maughan@gmail.com>
2016-09-23 12:54:10 -0700

    third commit

---
+++ thirtyone.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+	for (i, j) in (1..30).enumerate(){
+		println!("i ={} and j ={}", i, j);
+	}
+	
+	for (a, b) in (2..10).enumerate() {
+		println!("a is {} and b is {}", a, b);
+	}
+
+
+
+
+}
