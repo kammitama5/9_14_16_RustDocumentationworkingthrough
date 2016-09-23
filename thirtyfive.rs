5677302ba57e6151ab01dc0d7ae314e488553193
kammitama5 <krystal.maughan@gmail.com>
2016-09-23 13:19:54 -0700

    third commit

---
+++ thirtyfive.rs
@@ -0,0 +1,16 @@
+fn main()
+{
+	let v = vec![1, 2, 3, 4, 5]; // v : Vec<i32>
+	
+	println!("The third element of v is {}", v[2]);
+	
+	let x = vec![0; 10]; // x 
+	//vector containing ten zeros
+	println!("The fourth element of x is {}", x[3]);
+
+
+
+
+
+
+}
