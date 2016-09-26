f773d3663c1b5a08a13e706c79dceee7422b19fc
kammitama5 <krystal.maughan@gmail.com>
2016-09-26 07:46:19 -0700

    fourth commit

---
+++ thirty_nine.rs
@@ -0,0 +1,18 @@
+fn main(){
+
+
+	fn  take(v: Vec<i32>){
+	
+	
+}
+
+	let v = vec![1, 2, 3];
+	
+	//take(v);
+	
+	println!("v[0] is: {}", v[0]);
+	println!("v[1] is: {}", v[1]);
+	println!("v[2] is: {}", v[2]);
+
+
+}
