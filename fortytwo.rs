6e587a7d091ed007de97a658d46233b5f8b4bf4f
kammitama5 <krystal.maughan@gmail.com>
2016-09-26 08:29:15 -0700

    fourth commit

---
+++ fortytwo.rs
@@ -0,0 +1,28 @@
+fn main(){
+	
+	fn sum_vec(v: &Vec<i32>) -> i32 {
+		return v.iter().fold(0, |a, &b| a + b);
+	}
+	
+	fn foo(v1: &Vec<i32>, v2: &Vec<i32>) -> i32 {
+		let s1 = sum_vec(v1);
+		let s2 = sum_vec(v2);
+		
+		println!("s1:{}, s2:{}",s1, s2);
+		s1 + s2
+	}
+	
+	
+	
+	let v1 = vec![1, 2, 3];
+	let v2 = vec![4, 5, 6];
+	
+	let answer = foo(&v1, &v2);
+	println!("{}", answer);
+
+
+
+
+
+
+}
