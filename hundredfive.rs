c4b9521f01f9afadd46a5b3591fcbdbd1ff7c7e2
kammitama5 <krystal.maughan@gmail.com>
2016-10-05 11:48:04 -0700

    8th commit

---
+++ hundredfive.rs
@@ -0,0 +1,11 @@
+fn main(){
+	let x = "Hello";
+	let y:i32 = 56;
+	let mut my_variable = "bye";
+	my_variable = "what's up?";
+	
+	println!("x: {}", x);
+	println!("y: {}", y);
+	println!("my_variable = {}", my_variable);
+
+}
